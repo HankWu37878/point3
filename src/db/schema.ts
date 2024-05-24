83791adf7b91f67f5a0e98634f72b106bc838e35
Jing-Cheng <jenseniu0810@gmail.com>
2024-05-24 20:01:59 +0800

    finish schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -13,15 +13,19 @@ import {
 export const ReservationTable = pgTable(
     "Reservation",
     {
-    id: serial("id").primaryKey(),
-    displayName: varchar("name").notNull(),
-    email: varchar("email"),
-    Date: varchar("date", {length: 15}).notNull(),
-    startTime: varchar("startTime").notNull(),
-    endTime: varchar("endTime").notNull(),
-    roomId: varchar("roomId").notNull()
+        id: serial("id").primaryKey(),
+        displayName: varchar("name").notNull(),
+        email: varchar("email"),
+        Date: varchar("date", {length: 15}).notNull(),
+        startTime: varchar("startTime").notNull(),
+        endTime: varchar("endTime").notNull(),
+        roomId: varchar("roomId").notNull()
     },
-
+    (table) => ({
+        emailIndex: index("email_index").on(table.email),
+        roomIndex: index("room_index").on(table.roomId),
+        uniqReservation: unique().on(table.roomId, table.Date, table.startTime, table.endTime),
+    })
 );
 
 //email, name, time, which room, date
